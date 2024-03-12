77e801f9615efda6e504b25c7f03bbd9650deada
TlahuicoleSystem <barrales.josue.1i@gmail.com>
2024-03-12 10:14:07 -0600

    [Josue] createProject

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hola Mundo, mamaaaaaa estoy en nextJS</h1>
+    </div>
+  );
+}
